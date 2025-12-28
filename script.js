8f82a8b20aeb3c49f7a2ffa73b19909b2b940151
khadidja20232024 <155075654+khadidja20232024@users.noreply.github.com>
2025-12-28 22:58:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const forms = document.querySelectorAll('form[action="cart.php"] input[name="add"]');
+    document.querySelectorAll('.add-btn').forEach(btn => {
+        btn.addEventListener('click', () => {
+            const id = btn.dataset.id;
 
-    forms.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Product added to cart!');
+            fetch('add_to_cart.php', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                body: 'product_id=' + id
+            })
+            .then(res => res.text())
+            .then(() => {
+                showMessage('Item added to cart');
+            });
         });
     });
 });
 
+function showMessage(text) {
+    let msg = document.createElement('div');
+    msg.className = 'success-msg';
+    msg.innerText = text;
+    document.body.prepend(msg);
 
+    setTimeout(() => {
+        msg.remove();
+    }, 2000);
+}
